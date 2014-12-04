597deac5b4d84ad9136d96127cc9a5dc4995a4ab
Gary Taylor <gary.taylor@hismessages.com>
2014-12-04 14:39:23 +0000

    Released v0.1.5 - to include seed data to get the user started

---
+++ db/seed.rb
@@ -0,0 +1,26 @@
+require 'agile_proxy/model/user'
+require 'agile_proxy/model/application'
+module AgileProxy
+  class Seed
+    class << self
+      def load_seed
+        create_default_user
+        create_default_application
+      end
+
+      private
+
+      def create_default_user
+        User.create name: 'public', email: 'public@agileproxy.com', id: 1 if (User.where(name: 'public').count == 0)
+      end
+
+      def create_default_application
+        Application.create user_id: public_user.id, name: 'Default Application', username: 'public-app-1', password: 'password', id: 1 if (Application.where(name: 'Default Application').count == 0)
+      end
+
+      def public_user
+        User.where(name: 'public').first
+      end
+    end
+  end
+end
